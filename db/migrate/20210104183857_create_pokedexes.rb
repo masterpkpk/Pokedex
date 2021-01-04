c8f2a5fc96502847d7384f41454edfcd1b4534b8
william <masterpkpk@gmail.com>
2021-01-04 13:49:04 -0500

    added name to pokedex

--- db/migrate/20210104183857_create_pokedexes.rb
+++ db/migrate/20210104183857_create_pokedexes.rb
@@ -1,7 +1,7 @@
 class CreatePokedexes < ActiveRecord::Migration
   def change
-    create_table :pokedexes do |t|
-
+    create_table :pokedexes do |t|    
+      t.string :name
       t.timestamps null: false
     end
   end
