61a676a6d633e9d387cd12e72d86066f84b52215
william <masterpkpk@gmail.com>
2021-01-04 17:21:24 -0500

    added route for home. allows users to sign up made changes to welcome and home pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,36 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :welcome 
   end
+
+  post "/signup" do 
+    @trainer = Trainer.create(username: params[:username], password_digest: params[:password])
+    session[:user_id] = @trainer.id
+    redirect "/home"
+  end 
+
+  get '/home' do
+    @trainer = Trainer.find_by_id(session[:user_id])
+    if logged_in?
+      erb :home
+    else
+      redirect "/"
+    end
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= Trainer.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      current_user != nil
+    end
+  end
+
+
+
+
 
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,8 @@
 class Trainer < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :username, :password_digest
+  
   has_many :pokedexes
-  has_many :pokemon, through: :pokedexes
+  has_many :pokemon
 
 end
