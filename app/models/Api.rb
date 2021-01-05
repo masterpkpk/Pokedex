a6cb274a102f2beb92a8cd5b1d8ac518aa4cc021
william <masterpkpk@gmail.com>
2021-01-05 11:35:00 -0500

    Api creates pokemons

--- app/models/Api.rb
+++ app/models/Api.rb
@@ -9,24 +9,38 @@ class Api
       @pokemon = @pokemon_data["results"].collect do |pokemon|
         pokemon
       end
+      @pokemon.each do |pokemon|
+        
+        pokemons = "https://pokeapi.co/api/v2/pokemon/#{pokemon["name"]}"
+       
+        poke_uri = URI.parse(pokemons)
+        responses = Net::HTTP.get_response(poke_uri)
+        responses.body
+        
+        pokemon_data = JSON.parse(responses.body)
+        
+        
+        Pokemon.create(name: pokemon_data["name"])
+        
+      end
 
      
     end
 
     def pokemon_new
-      self.map do |pokemon|
-        pokemon
-      end  
+      
+      
+      
         
         
         
         
-        # pokemon = "https://pokeapi.co/api/v2/#{pokemon["name"]}"
-        # poke_uri = URI.parse(pokemon)
-        # response = Net::HTTP.get_response(poke_uri)
-        # response.body
-        # pokemon_data = JSON.parse(response.body)
-        # Pokemon.new(name: pokemon_data["name"], type: pokemon_data["types"][0]["type"]["name"])
+        pokemon = "https://pokeapi.co/api/v2/#{pokemon["name"]}"
+        poke_uri = URI.parse(pokemon)
+        response = Net::HTTP.get_response(poke_uri)
+        response.body
+        pokemon_data = JSON.parse(response.body)
+        Pokemon.new(name: pokemon_data["name"], type: pokemon_data["types"][0]["type"]["name"])
 
       
 
