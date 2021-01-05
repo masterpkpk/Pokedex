6c22c7dd115516d19a95734d25668591f66efa8b
william <masterpkpk@gmail.com>
2021-01-05 15:33:35 -0500

    added uniqueness to username

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,6 @@
 class Trainer < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :password_digest
+  validates_presence_of :username, :password_digest
   validates :username, presence: true, uniqueness: true
 
   
