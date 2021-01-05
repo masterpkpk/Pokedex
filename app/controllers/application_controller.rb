7cfe958ab6748d5956189cb16c349144706026c2
william <masterpkpk@gmail.com>
2021-01-05 15:29:40 -0500

    added ability to delete user , made changes to erb pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
-require './config/environment'
+require "./config/environment"
 
 class ApplicationController < Sinatra::Base
 
   configure do
     enable :sessions
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
     set :session_secret, "hotdog"
   end
 
@@ -14,35 +14,52 @@ class ApplicationController < Sinatra::Base
     erb :welcome 
   end
 
+  get '/signup' do
+    erb :signup
+  end
+
   post "/signup" do 
     @trainer = Trainer.create(username: params[:username], password: params[:password])
     session[:user_id] = @trainer.id
     redirect "/home"
   end 
 
+  get '/login' do
+    erb :login
+  end
+
   post "/login" do
     user = Trainer.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+     
       redirect "/home"
     else
       redirect "/"
     end
   end
 
-  post '/logout' do
+  post "/logout" do
     session.clear
-    redirect '/'
+    redirect "/"
   end
   
-  get '/home' do
+  get "/home" do
     
-    @trainer = Trainer.find_by_id(session[:user_id])
+    @trainer = current_user
+    
     if logged_in?
       erb :home
+      
     else
       redirect "/"
     end
+  end
+
+  post "/delete/:user_id" do
+    Trainer.destroy(params[:user_id])
+    redirect '/'
+   
   end
 
 
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,6 +1,8 @@
 class Trainer < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :username, :password_digest
+  validates_presence_of :password_digest
+  validates :username, presence: true, uniqueness: true
+
   
   has_many :pokedexes
   has_many :pokemon
